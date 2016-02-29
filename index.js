19b0b27af1cc83fc865afcc943102a4011474266
j- <jh@live.com.au>
2016-02-29 11:34:08 +1100

    Add rule setting values

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// 0 - turn the rule off
+const OFF = 0;
+// 1 - turn the rule on as a warning (doesn't affect exit code)
+const WARN = 1;
+// 2 - turn the rule on as an error (exit code is 1 when triggered)
+const ERROR = 2;
+
 module.exports = {
 
 	globals: {
