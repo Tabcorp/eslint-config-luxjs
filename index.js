5c7807d18f129ffd2d72421c6f3e4f3fab012b1e
j- <j@skeoh.com>
2016-03-04 16:30:47 +1100

    Ensure no duplicate arguments are defined outside strict mode

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = {
 		// Possible Errors
 		'no-console': [ERROR],
 		'no-debugger': [ERROR],
+		'no-dupe-args': [ERROR],
 		// Stylistic Issues
 		'array-bracket-spacing': [ERROR, 'never'],
 		'block-spacing': [ERROR, 'always'],
